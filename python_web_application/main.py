8352a94113dab5decf1a9e80bf3f22b2cd467608
Naomasa Murata <murata-n@kadokawa.jp>
2021-04-28 11:46:48 +0900

    feat: とりあえず必要そうなページ部分の雛形を置いた

--- python_web_application/main.py
+++ python_web_application/main.py
@@ -8,6 +8,24 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
+@app.get("/")
+async def read_root_page(request: Request):
+    """一覧を表示する"""
+    return {"message": "TODO"}
+
+
+@app.get("/add")
+async def read_post_page(request: Request):
+    """新規アイテム追加ページの表示"""
+    return {"message": "TODO"}
+
+
+@app.post("/add")
+async def post_new_item(request: Request):
+    """新規アイテムの追加"""
+    return {"message": "TODO"}
+
+
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
