bcea463c0e07df6bd2039f927ead516392526ade
Naomasa Murata <murata-n@kadokawa.jp>
2021-04-28 11:24:35 +0900

    feat: アイテムページの追加
    hello world的なのしただけ

--- python_web_application/main.py
+++ python_web_application/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
