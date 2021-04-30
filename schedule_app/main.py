299864f84dc584466145603feb8c60edd503c2b3
Naomasa Murata <murata-n@kadokawa.jp>
2021-04-30 11:14:39 +0900

    feat: バック側でも制約確認

--- schedule_app/main.py
+++ schedule_app/main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 import sqlalchemy as sqla
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request, status
 from fastapi.param_functions import Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.responses import Response
 
 from schedule_app import db
 from schedule_app.config import Settings
@@ -51,6 +52,7 @@ async def read_post_page(request: Request):
 @app.post("/add")
 async def post_new_item(
     request: Request,
+    response: Response,
     engine: sqla.engine.Connectable = Depends(get_engine),
     title: str = Form(""),
     body: str = Form(""),
@@ -58,8 +60,18 @@ async def post_new_item(
     end_at: str = Form(None),
 ):
     """新規アイテムの追加"""
+    if not title or not begin_at or not end_at:
+        # 必須データが足りなかったらエラーを返す
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"msg": "error"}
+
     beginAt = string2Datetime(begin_at)
     endAt = string2Datetime(end_at)
+
+    if beginAt > endAt:
+        # 開始日時より終了日時が早かったらエラーを返す
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"msg": "error"}
 
     db.add_item(
         engine, Schedule(title=title, body=body, begin_at=beginAt, end_at=endAt)
